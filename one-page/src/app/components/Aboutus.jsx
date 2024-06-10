2c0a4f6f7bc39d360cfdda0147d3d080408a32a9
Diogo Maia <151968564+AyoMaia@users.noreply.github.com>
2024-06-10 17:08:03 -0300

    edit

--- one-page/src/app/components/Aboutus.jsx
+++ one-page/src/app/components/Aboutus.jsx
@@ -10,7 +10,7 @@ export default function Aboutus() {
 
         <div className='w-1/2 items-center flex flex-col justify-center p-8'>
           <h3 className='text-center text-white text-xl font-semibold'>Lorem ipsum dolor sit amet, consectetur adipisicing elit.</h3>
-          <p className='text-gray-300 mt-8 text-center'>Lorem ipsum dolor sit amet consectetur adipisicing elit. Iusto quae reprehenderit veritatis? Harum, ut dolorem vero quos iusto id ipsum at rerum veniam? Earum modi magni minima eius officiis voluptatum.</p>
+          <p className='text-gray-300 mt-8 text-center'>Lorem ipsum dolor sit amet consectetur adipisicing elit. Iusto quae reprehenderit veritatis? Harum, ut dolorem vero quos iusto id ipsum at rerum veniam? Earum modi magni minima eius.</p>
           <button className='transition ease-in-out delay-150 bg-blue-500 hover:-translate-y-1 hover:scale-110 hover:bg-indigo-900 duration-300 py-4 px-8 mt-10 text-white text-xl shadow-xl'>Contact</button>
         </div>
       </section>
